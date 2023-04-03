415451855f750339c91467cbfed0670bd92f54f0
wjh <bitwjh99@gmail.com>
2023-04-03 16:53:42 +0800

    update

--- doc/StoreMap合约文件改动记录.md
+++ doc/StoreMap合约文件改动记录.md
@@ -1,13 +1,14 @@
 > 原始的`StoreMap.sol`在[这里](../reference/investigation-cjzhuang2020/cjz_underg_2021_09/tree_blockchain/contracts/StoreMap.sol)  
+> 改动后的合约文件在`src/contract_on_system`目录下
 ### 改动记录
 > `新增`或`修改`类型的改动位置为新文件，`删除`类型的改动位置为旧文件  
 
 |改动类型|改动位置|描述|源文件|新文件|
 | :---: | :---: | :---: | :---: | :---: |
-|新增|-|增加了[SPDX许可证](https://docs.soliditylang.org/en/v0.8.19/layout-of-source-files.html#spdx-license-identifier)|-|//SPDX-License-Identifier: MIT|
-|修改|-|修改因[版本问题](https://docs.soliditylang.org/en/v0.8.19/layout-of-source-files.html#pragmas)引起的语法错误|pragma solidity ^0.5.16;|pragma solidity >= 0.5.16;|
-|修改|-|修改了未知原因的[类型声明](https://docs.soliditylang.org/en/v0.8.19/types.html#fixed-size-byte-arrays)问题|byte c = bytes(geohash)[i];|bytes1 c = bytes(geohash)[i];|
-|修改|-|同上|byte c = bytes(geohash)[i];|bytes1 c = bytes(geohash)[i];|
+|新增|-|增加了[SPDX许可证](https://docs.soliditylang.org/en/v0.8.19/layout-of-source-files.html#spdx-license-identifier)|-|`//SPDX-License-Identifier: MIT`|
+|修改|-|修改因[版本问题](https://docs.soliditylang.org/en/v0.8.19/layout-of-source-files.html#pragmas)引起的语法错误|`pragma solidity ^0.5.16`|`pragma solidity >= 0.5.16`|
+|修改|-|修改了未知原因的[类型声明](https://docs.soliditylang.org/en/v0.8.19/types.html#fixed-size-byte-arrays)问题|`byte c = bytes(geohash)[i]`|`bytes1 c = bytes(geohash)[i]`|
+|修改|-|同上|`byte c = bytes(geohash)[i]`|`bytes1 c = bytes(geohash)[i]`|
 |修改|-|将int类型统一为uint32|-|-|
 |修改|-|修改不便于理解的变量名|-|-|
 |分离|[Navigation](../src/contract_on_system/Navigation.sol)|将导航从地图存储中分离|-|-|
--- src/contract_on_system/uploadmap.js
+++ src/contract_on_system/uploadmap.js
@@ -37,7 +37,7 @@ var output = JSON.parse(
 var storeMap_abi = output.contracts['StoreMap.sol']['StoreMap'].abi
 var storeMap_bytecode = output.contracts['StoreMap.sol']['StoreMap'].evm.bytecode.object
 
-//部署合约
+//合约部署函数
 var web3 = require('web3')		//此处使用的是读者个人的web3包，是使用npm下载的
 async function deploy(account){
     var storeMapContract = new web3.eth.Contract(storeMap_abi)
@@ -61,22 +61,24 @@ async function deploy(account){
 
     return storeMapInstance
 }
-var accounts = await web3.eth.getAccounts()
-var storeMapInstance = await deploy(accounts[0])
 
+//获取账户
+var accounts = await web3.eth.getAccounts()
+//部署合约
+var storeMapInstance = await deploy(accounts[0])
 //上传地图
 uploadMap();
 
 //counter计数器用于记录当前已经上传成功的元素数
-let counter = 0;
+var counter = 0;
 function uploadMap() {
-	let partTask = [];
+	var partTask = [];
     //loopNum用于设定每个区块上链的交易数量（每个交易是一次元素上链函数的调用，由于交易提交速度远快于出块速度，所以基本上是每块200个交易）
-	let loopNum = 200;
+	var loopNum = 200;
 
     //上传loopNum个元素
-	for (let i = 0; i < loopNum; i++) {
-		partTask.push(add_one_element(elements[counter]));
+	for (var i = 0; i < loopNum; i++) {
+		partTask.push(add_one_element(elements[counter]));		//一个元素一个元素地上传
 		counter++;
         //如果上传完成counter == elements.length
 		if ((elements.length - counter) == 0) {
@@ -88,7 +90,7 @@ function uploadMap() {
 			return;
 		}
 	}
-    //上传loopNum个元素之后继续上传
+    //上传loopNum个元素之后（一个块上链）继续上传
 	if (partTask.length == loopNum) {
 		Promise.all(partTask).then((resolve) => {
 			uploadMap();
@@ -114,31 +116,28 @@ async function add_one_element(element) {
 	var gas = 3000000;
 	var path_length = 0;
 	var path = [];
-	// 判断是否是多边形
-	// console.log(element_json.geometry.type);
-	if (element_json.geometry.type == "Point") {
-		minzoom = 14;
+
+	if (element_json.geometry.type == "Point") {		//如果是一个点元素
+		minzoom = 14;									//点元素14缩放才能显示
+		//将点的经纬度坐标转换成geohash
 		var point_x = parseFloat(path_arr[0]);
 		var point_y = parseFloat(path_arr[1]);
 		var point_geohash = encode_geohash(point_x, point_y, 11);
-		path.push(point_geohash);
-		gas += 110000;
-		// console.log("onepoint",point_geohash);
+		path.push(point_geohash);						//一个点的path只有一个值
+		//上链的时候要把path拷贝进区块，path越长消耗的汽油费越多。为每个元素的拷贝提供11000汽油费
+		gas += 110000;									//点元素只加一次
 	}
-	else if (element_json.geometry.type == "LineString") {
-		minzoom = 7;
-		path_length = path_arr.length;
-		// console.log(path_length);
-		for (var i = 0; i < path_length; i++) {
-			path.push(path_arr[i]);
-			gas += 110000;
-			// console.log("onepoint",point_geohash);
-		}
+	else if (element_json.geometry.type == "LineString") {	//如果是一个线元素
+		minzoom = 7;										//线元素7缩放就显示
+		//把线元素的路径点添加到path中，LineString的cordinates就是一个基本类型一维数组，直接复制
+		path = path_arr.slice()
+		//增加汽油费
+		gas += 110000 * path_arr.length;
 	}
-	else if (element_json.geometry.type == "MultiPolygon") {
-		minzoom = 12;
+	else if (element_json.geometry.type == "MultiPolygon") {	//如果是重多边形
+		minzoom = 12;											//冲多边形12缩放显示
+		//此处笔者不太理解
 		path_length = path_arr[0][0].length;
-		// console.log(path_length);
 		for (var i = 0; i < path_length; i++) {
 			var point = path_arr[0][0][i];
 			var point_x = parseFloat(point[0]);
@@ -181,7 +180,7 @@ async function add_one_element(element) {
 
 	gid = element_json.properties.gid;
 	// 将地图信息存储到区块链
-	await add_one_element(gid, minzoom, cost, source, target, oneway, building, highway, name, element_json.geometry.type, path, gas, 0);
+	await add_element_to_blockchain(gid, minzoom, cost, source, target, oneway, building, highway, name, element_json.geometry.type, path, gas);
 	// console.log(counter,minzoom,oneway,building,highway,name,element_json.geometry.type,path);
 
 	// console.log(path);
@@ -205,7 +204,7 @@ async function bind_road_geohash(gid, path) {
 		}
 		if (hash_array.indexOf(area_geohash5) == -1) {
 			hash_array.push(area_geohash5);
-			await add_area_element(area_geohash5, gid, 5000000);
+			await bind_element(area_geohash5, gid, 5000000);
 			console.log("area_geohash5", area_geohash5);
 		}
 	}
@@ -215,20 +214,20 @@ async function bind_road_geohash(gid, path) {
 async function bind_other_geohash(gid, path) {
 	// 对于Point和MultiPolygon，直接根据前缀绑定
 	for (var i = 0; i < path.length; i++) {
-		var area_geohash4 = path[i].slice(0, 4);
+		var area_geohash4 = path[i].slice(0, 4)
 		if ((hash_array.indexOf(area_geohash4) != -1)) {
 			continue;
 		}
 		if (hash_array.indexOf(area_geohash4) == -1) {
 			hash_array.push(area_geohash4);
-			await add_area_element(area_geohash4, gid, 5000000);
+			await bind_element(area_geohash4, gid, 5000000);
 			console.log("area_geohash4", area_geohash4);
 		}
 	}
 }
 
 //向指定geohash区域内添加一个元素
-async function add_area_element(geohash, gid, gas) {
+async function bind_element(geohash, gid, gas) {
     var retry_time = 0
     while(retry_time < 10){
         await storeMapInstance.method.add_area_element(geohash, gid).send({
@@ -238,38 +237,48 @@ async function add_area_element(geohash, gid, gas) {
             txtime: 278000 
          })
          .on('transactionhash', (hash) => {
-            console.log("transaction submit! geohash: " + geohash + ", gid: " + gid)
+            console.log("bind transaction submit! geohash: " + geohash + ", gid: " + gid)
          })
          .on('receipt', (receipt) => {
-            console.log("mined! geohash: " + geohash + ", gid: " + gid + " At " + receipt.blockNumber)
+            console.log("bind mined! geohash: " + geohash + ", gid: " + gid + " At " + receipt.blockNumber)
          })
          .on('error', (err) => {
-            console.log("bind road failed. geohash: " + geohash + " ,gid: " + gid);
+            console.log("bind failed. geohash: " + geohash + " ,gid: " + gid);
             console.log('err: ' + err)
             console.log("retry!")
             retry_time++
          })
     }
     //重试10此均未成功
-	console.log("bind road failed. geohash: " + geohash + " ,gid: " + gid);
+	console.log("bind failed! geohash: " + geohash + " ,gid: " + gid);
 	return;
 }
 
-async function add_one_element(gid, minzoom, cost, source, target, oneway, building, highway, name, gtype, path, gas, retry_times) {
-	if (retry_times > 10) {
-		console.log("add element failed. gid: " + gid);
-		return;
-	}
-	// console.log("source, target", source, target);
-	await storeMapInstance.add_one_element(gid, minzoom, cost, source, target, oneway, building, highway, name, gtype, path, { from: accounts[0], gas: gas, position: "w3511111111111", txtime: 278000 }).then(function (result) {
-		console.log("gid: ", gid);
-		console.log("path: ", path)
-	},
-		function (err) {
-			//retry
-			console.log("add_one_element err: ", err)
-			add_one_element(gid, minzoom, cost, source, target, oneway, building, highway, name, gtype, path, gas, retry_times + 1);
-		});
+async function add_element_to_blockchain(gid, minzoom, cost, source, target, oneway, building, highway, name, gtype, path, gas) {
+	var retry_time = 0
+    while(retry_time < 10){
+        await storeMapInstance.method.add_one_element(gid, minzoom, cost, source, target, oneway, building, highway, name, gtype, path).send({
+            from: accounts[0],
+            gas: gas,
+            position: "w3511111111111",
+            txtime: 278000
+         })
+         .on('transactionhash', (hash) => {
+            console.log("add transaction submit! name: " + name + ", gid: " + gid)
+         })
+         .on('receipt', (receipt) => {
+            console.log("add mined! name: " + name + ", gid: " + gid + " At " + receipt.blockNumber)
+         })
+         .on('error', (err) => {
+            console.log("add failed. name: " + name + " ,gid: " + gid);
+            console.log('err: ' + err)
+            console.log("retry!")
+            retry_time++
+         })
+    }
+    //重试10此均未成功
+	console.log("add failed! name: " + name + " ,gid: " + gid);
+	return;
 }
 
 // var precision = 6;
